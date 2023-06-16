32b51a4796d536e091ecffa21a0cde188318c10c
hoangtrung020541 <hoangtrung020541@gmail.com>
2023-06-16 06:50:11 +0000

    fix: bleu score

--- src/models/pali/metrics.py
+++ src/models/pali/metrics.py
@@ -12,37 +12,37 @@ scorers = [(Cider(), "CIDEr")]
 def argmax_logits(logits):
     return jnp.argmax(logits, axis=-1)
 
-def bleu_score(logits, targets):
-    mean_blue_score = 0
-    predicted_tokens = argmax_logits(logits)
-    def single_bleu_score(l, t):
-        ref = vocab_encoder_decoder.decode(t)
-        hypo = vocab_encoder_decoder.decode(l)
-        print(f"Y: {ref} | Yhat: {hypo}")
+
+def convert_to_text(logit_tokens, target_tokens):
+    hypothesis = vocab_encoder_decoder.decode(logit_tokens)
+    references = vocab_encoder_decoder.decode(target_tokens)
+    # print(f"Y: {references} | Yhat: {hypothesis}")
+    return hypothesis, references
+
+
+def bleu_score(logits, target_tokens):
+    mean_blue_score = 0.0
+    logit_tokens = argmax_logits(logits)
+    for l, t in zip(logit_tokens, target_tokens):
+        hypothesis, references  = convert_to_text(l, t)
         try:
-            result = google_bleu.compute(predictions=[hypo], references=[[ref]])
+            result = google_bleu.compute(predictions=[hypothesis], references=[[references]])
             result = result['google_bleu']
         except:
             result=0.0
-        return result
-    for l, t in zip(predicted_tokens, targets):
-        mean_blue_score += single_bleu_score(l, t)
+        mean_blue_score += result
     return mean_blue_score / len(logits)
 
-def cider_score(logits, targets):
-    # shape(logits): (batch size, 18, 32128) - shape(target): (batch size, 18)
+
+def cider_score(logits, target_tokens):
     final_scores = {}
-    # shape(predicted_tokens): (3,18)
-    predicted_tokens = argmax_logits(logits)
-    def convert_to_text(logit, target):
-        ref = vocab_encoder_decoder.decode(target)
-        hypo = vocab_encoder_decoder.decode(logit)
-        # print(f"Y: {ref} | Yhat: {hypo}")
-        return ref, hypo
     y_arr = []
     y_hat_arr = []
-    for logit, target in zip(predicted_tokens, targets):
-        y, y_hat = convert_to_text(logit, target)
+    # shape(logits): (batch size, 18, 32128) - shape(target): (batch size, 18)
+    # shape(logit_tokens): (3,18)
+    logit_tokens = argmax_logits(logits)
+    for l, t in zip(logit_tokens, target_tokens):
+        y_hat, y = convert_to_text(l, t)
         y_arr.append(y)
         y_hat_arr.append(y_hat)
     # shape(y_hat_arr and y_arr): (batch size,)
@@ -60,30 +60,26 @@ def cider_score(logits, targets):
             final_scores[method] = score  
     return final_scores 
 
-def combine_metrics(logits, targets):
-    # shape(logits): (batch size, 18, 32128) - shape(target): (batch size, 18)
+
+def combine_metrics(logits, target_tokens):
     cider_scores = {}
-    mean_blue_score = 0
-    predicted_tokens = argmax_logits(logits)
-    # shape(predicted_tokens): (3,18)
-    predicted_tokens = argmax_logits(logits)
-    def convert_to_text(logit, target):
-        ref = vocab_encoder_decoder.decode(target)
-        hypo = vocab_encoder_decoder.decode(logit)
-        # print(f"Y: {ref} | Yhat: {hypo}")
-        try:
-            bleu_result = google_bleu.compute(predictions=[hypo], references=[[ref]])
-            bleu_result = result['google_bleu']
-        except:
-            bleu_result = 0.0
-        return ref, hypo, bleu_result
+    mean_blue_score = 0.0
     y_arr = []
     y_hat_arr = []
-    for logit, target in zip(predicted_tokens, targets):
-        y, y_hat, bleu = convert_to_text(logit, target)
+    # shape(logits): (batch size, 18, 32128) - shape(target): (batch size, 18)
+    # shape(logit_tokens): (3,18)
+    logit_tokens = argmax_logits(logits)
+    for l, t in zip(logit_tokens, target_tokens):
+        y_hat, y = convert_to_text(l, t)
         y_arr.append(y)
         y_hat_arr.append(y_hat)
-        mean_blue_score = mean_blue_score + bleu
+        # print(f"Y: {y} | Yhat: {y_hat}")
+        try:
+            bleu_result = google_bleu.compute(predictions=[y_hat], references=[[y]])
+            bleu_result = bleu_result['google_bleu']
+        except:
+            bleu_result = 0.0
+        mean_blue_score += bleu_result
     bleu_score = mean_blue_score / len(logits)
     # shape(y_hat_arr and y_arr): (batch size,)
     # convert the shape of y_arr to (batch size, 1)
